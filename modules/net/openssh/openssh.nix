1421efca9efa291230359ebedd7719c867ebcc03
suazo-dev <me@suazo.dev>
2026-05-10 13:11:46 -0400

    fix open ssh bug 2

--- modules/net/openssh/openssh.nix
+++ modules/net/openssh/openssh.nix
@@ -2,27 +2,29 @@
 let
   isDarwin = lib.hasSuffix "-darwin" spec.system;
 in {
-  services.openssh.enable = true;
-
-  services.openssh.settings = lib.mkIf (!isDarwin) {
-    PermitRootLogin = "no";
-    PasswordAuthentication = false;
-    KbdInteractiveAuthentication = false;
-    PubkeyAuthentication = true;
-    X11Forwarding = false;
-    TCPKeepAlive = true;
-    ClientAliveInterval = 30;
-    ClientAliveCountMax = 6;
+  services.openssh = {
+    enable = true;
+  } // lib.optionalAttrs (!isDarwin) {
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PubkeyAuthentication = true;
+      X11Forwarding = false;
+      TCPKeepAlive = true;
+      ClientAliveInterval = 30;
+      ClientAliveCountMax = 6;
+    };
+  } // lib.optionalAttrs isDarwin {
+    extraConfig = ''
+      PermitRootLogin no
+      PasswordAuthentication no
+      KbdInteractiveAuthentication no
+      PubkeyAuthentication yes
+      X11Forwarding no
+      TCPKeepAlive yes
+      ClientAliveInterval 30
+      ClientAliveCountMax 6
+    '';
   };
-
-  services.openssh.extraConfig = lib.mkIf isDarwin ''
-    PermitRootLogin no
-    PasswordAuthentication no
-    KbdInteractiveAuthentication no
-    PubkeyAuthentication yes
-    X11Forwarding no
-    TCPKeepAlive yes
-    ClientAliveInterval 30
-    ClientAliveCountMax 6
-  '';
 }
